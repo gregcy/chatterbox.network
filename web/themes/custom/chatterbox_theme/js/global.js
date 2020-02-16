8c0250fafefa757f09f8900735670672963623a2
Greg Andreou <greg.andreou@gmail.com>
2020-02-16 16:51:03 +0200

    Created deticated theme and updates

---
+++ web/themes/custom/chatterbox_theme/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.chatterbox_theme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
