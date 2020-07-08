a056ef09edcbc791bd5ae369e096c9e7693b83d2
Greg Andreou <greg.andreou@gmail.com>
2020-07-08 21:35:20 +0300

    Figured out how to customise Rest output

---
+++ web/modules/custom/chatterbox_pothen_esxes/src/Normalizer/PothenEsxesNodeEntityNormalizer.php
@@ -0,0 +1,44 @@
+<?php
+
+  namespace Drupal\chatterbox_pothen_esxes\Normalizer;
+
+  use Drupal\serialization\Normalizer\ContentEntityNormalizer;
+  use Drupal\node\NodeInterface;
+
+/** 
+* Converts the Drupal entity object structures to a normalized array. 
+*/
+class PothenEsxesNodeEntityNormalizer extends ContentEntityNormalizer {
+  /** 
+  * The interface or class that this Normalizer supports. 
+  *  @var string 
+  */
+  protected $supportedInterfaceOrClass = 'Drupal\node\NodeInterface';
+   
+  public function supportsNormalization($data, $format = NULL) {
+    // If we aren't dealing with an object or the format is not supported return now.
+    if (!is_object($data) || !$this->checkFormat($format)) {
+      return FALSE;
+    }
+    // This custom normalizer should be supported for "Pothen Esxes" nodes.
+    if ($data instanceof NodeInterface && $data->getType() == 'pothen_esxes') {
+      return TRUE;
+    }
+    // Otherwise, this normalizer does not support the $data object.
+      return FALSE;
+  }
+    
+  /**
+  * {@inheritdoc} 
+  */
+  public function normalize($entity, $format = NULL, array $context = array()) {
+       
+    $new_attributes = array();
+    $title = $entity->getTitle();
+
+    $new_attributes['metadata'] = array('title' => $title);
+
+    // Return the $attributes with our new values.
+    return $new_attributes;
+  }
+}
